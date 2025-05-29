c0bc954acfe3867617c1d4da836c1f842d7422bc
thlee <lmnxyz271@gmail.com>
2025-05-29 23:54:06 +0900

    dto

--- book-management-project/backend/src/main/java/com/book/backend/dto/BookDTO.java
+++ book-management-project/backend/src/main/java/com/book/backend/dto/BookDTO.java
@@ -1,5 +1,8 @@
 package com.book.backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
