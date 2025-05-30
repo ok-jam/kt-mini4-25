49fa900d6405cf7d1c1d53de87c863c1241b08a0
thlee <thlee@gmail.com>
2025-05-30 11:22:15 +0900

    bookList 추가

--- book-management-project/backend/src/main/java/com/book/backend/controller/BookController.java
+++ book-management-project/backend/src/main/java/com/book/backend/controller/BookController.java
@@ -6,6 +6,7 @@ import com.book.backend.domain.Book;
 import com.book.backend.dto.BookDTO;
 import com.book.backend.service.BookService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,14 +18,20 @@ public class BookController {
 
     public final BookService bookService;
 
-    @GetMapping
-    public List<String> getBooks() {
-        return List.of("도서 1", "도서 2", "도서 3");
-    }
+//    @GetMapping
+//    public List<String> getBooks() {
+//        return List.of("도서 1", "도서 2", "도서 3");
+//    }
 
     @PostMapping
     public ResponseEntity<Book> bookRegist(@RequestBody BookDTO.BookRegist dto) {
         Book book = bookService.bookRegist(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(book);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookDTO.BookList>> bookList() {
+        List<BookDTO.BookList> books = bookService.bookList();
+        return ResponseEntity.ok(books);
     }
 }
